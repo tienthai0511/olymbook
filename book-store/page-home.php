11694aa8c3f3cd46026ea056f733f45ecc8ea166
Thaivt <tienthai0511@gmail.com>
2014-12-19 00:05:47 +0700

    add new layout
    add new layout

---
+++ book-store/page-home.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* Template Name: Home Page
+*/
+get_header(); ?>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$('#myCarousel').carousel({
+				interval: 100000
+			})
+
+		$('.carousel .item').each(function(){
+			var next = $(this).next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
+
+			if (next.next().length>0) {
+				next.next().children(':first-child').clone().appendTo($(this));
+			}
+			else {
+				$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+			}
+		});
+		});
+	</script>
+	 <section class="page-body-outer mt30">
+                   <section class="page-body-wrapper container "> 
+                         <div  style="max-width:1071px;">
+			<?php get_template_part( 'template-parts/book-store' ); ?>
+			</div>
+			<div  style="max-width:1071px;">
+			<?php get_template_part( 'template-parts/book-category' ); ?>
+			</div>
+			<div  style="max-width:1071px;">
+			<?php get_template_part( 'template-parts/book-new' ); ?>
+			</div>
+                  </section> <!--page-body-wrapper-end-->
+                </section>  
+
+<?php get_footer(); ?>
--- themes/twentythirteen/template-parts/book-store-area.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-	<div class="col-md-8 left-store">
-		<div class="price right">175K</div>s
-		<img src=""/>
-	</div>
-	<div class="col-md-4 right-store">
-	right
-	</div>
-</div>
