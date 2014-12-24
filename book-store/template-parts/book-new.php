26f322417e64f386f7281ca24f9b825c9071befe
Thaivt <tienthai0511@gmail.com>
2014-12-24 22:40:01 +0700

    fix

--- book-store/template-parts/book-new.php
+++ book-store/template-parts/book-new.php
@@ -5,9 +5,8 @@
 			<img src="<?php echo get_template_directory_uri(); ?>/images/book1.png"/>
 		</div>
 		<div class="content-block-bot text-middle">
-				
-				<a href="#">Dòng sách<span class="text-uppercase">Nghệ thuật lãnh dạo và quản ly</span></a>
-				<a class="right arrow-icon text-transparent" href="#">#</a>
+			<a href="#">Dòng sách<span class="text-uppercase">Nghệ thuật lãnh dạo và quản ly</span></a>
+			<a class="right arrow-icon text-transparent" href="#">#</a>
 			</div>
 	</div>
 	<div class="span6 block-new-item text-center">
@@ -16,69 +15,9 @@
 			<img src="<?php echo get_template_directory_uri(); ?>/images/book1.png"/>
 		</div>
 		<div class="content-block-bot text-middle">
-			
-			<a href="#">Dòng sách<span class="text-uppercase">Nghệ thuật lãnh dạo và quản ly</span></a>
+			<a href="#">Dòng sách <span class="text-uppercase">Nghệ thuật lãnh dạo và quản lý</span></a>
 			<a class="right arrow-icon text-transparent" href="#">#</a>
 		</div>
 	</div>
 	<div class="clearfix"></div>
 </div><!--/* end bock new-->
-<div class="row-fluid  block-store mt30">
-	<div class="span8 left-store">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/news.png"/>
-	</div>
-	<div class="span4 right-store">
-		<p class="t-tini text-uppercase">Tủ sách</p>
-		<h2 class="title-store text-uppercase text-justify">Tinh hoa lãnh đạo</h2>
-		<span class="author text-uppercase">John Kenl</span>
-		
-		<div class="content-store-text">
-			<p class="text-justify">With Bootstrap 2, we added optional mobile friendly styles for key aspects of the framework. With Bootstrap 3, we've rewritten the project to be mobile friendly from the start. Instead of adding on optional mobile styles, they're ba</p>
-			<p class="text-justify">With Bootstrap 2, we added optional mobile friendly styles for key aspects of the framework. With Bootstrap 3, we've rewritten the project to be mobile friendly from the start. Instead of adding on optional mobile styles, they're ba
-			</p>
-			<p>
-			nstead of adding on optional mobile styles, they're ba
-			</p>
-		</div>
-		<div class="clearfix"></div>
-		<div class="view-more-c">
-			<a href="#">Xem thêm </a>
-		</div>
-	</div>
-	<div class="clearfix"></div>
-	<div class=" height-75 nav-view-more widh-1075">
-		<a href="#" class="view-more">Xem thêm <span class="text-uppercase">Danh mục giải pháp tủ sách(10)</span></a>
-		<a href="#" class="btn-view-more">Xem thêm</a>
-	</div>
-</div>
-<div class="row-fluid  block-store block post mt30">
-	<div class="span8 left-block">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/bot-img.png"/>
-		<div class="block-post-top-img">
-			<h3 class="text-uppercase">7  bí quyết tạo động lực làm việc</h3>
-			<p class="text-justify">Trưa 20/12, dù sức khỏe còn yếu nhưng anh Nguyễn Văn Hường (30 tuổi, quê Nam Định) vẫn đòi bệnh viện đa khoa tỉnh Lâm Đồng cho ăn cơm. 
-			</p>
-			<a href="#" class="view-more-link right">Xem thêm</a>
-		</div>
-	</div>
-	<div class="span4 right-block pd-0">
-		<div class="item-block-post">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/item-post-img1.png"/>
-			<div class="block-post-item-img">
-			<h3 class="text-uppercase">7  bí quyết tạo động lực làm việc</h3>
-			<a href="#" class="view-more-link right">Xem thêm</a>
-		</div>
-		</div>
-		<div class="item-block post">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/item-post-img2.png"/>
-			<div class="block-post-item-img">
-			<h3 class="text-uppercase">7  bí quyết tạo động lực làm việc</h3>
-			<a href="#" class="view-more-link right">Xem thêm</a>
-		</div>
-	</div>
-	<div class="clearfix"></div>
-	<div class="height-75 nav-view-more widh-1075">
-		<a href="#" class="view-more">Xem thêm <span class="text-uppercase">Danh mục giải pháp tủ sách(10)</span></a>
-		<a href="#" class="btn-view-more">Xem thêm</a>
-	</div>
-</div>
