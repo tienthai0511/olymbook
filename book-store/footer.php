2dfdf6fd5a3c21487c36e4174bce29b0d37c857d
tienthai0511 <tienthai0511@gmail.com>
2015-01-06 10:32:01 +0700

    fix footer
    fix footer

--- book-store/footer.php
+++ book-store/footer.php
@@ -20,28 +20,29 @@
 				<div class="span7 static-position foot-info">
 				 <span>Khám phá và xem những thông tin quan trọng từ Olymbook.com</span>
 					<div class="footer-menu mt-20">
-						<ul>
-							<li>Hỗ trợ khách hàng</li>
-							<li>Khuyến mãi</li>
-							<li>Về Olymbook.com</li>
-							<li>Tài khoản của bạn</li>
-							<li>Block</li>
-							<li>Sitemap</li>
-						</ul>
+						 <?php wp_nav_menu( array( 'theme_location' => 'footer_menu' ) );  ?>
 						<div class="clearfix"></div>
 					</div>
 					<div class="mt-15 social-contain">
-					<span>&copy; Olymbook.com All rights reserved.</span>
+					<span>
+					<?php
+						if ($footer_text != '' ) {
+							echo sprintf(__('%s','crunchpress'), $footer_text);
+							}else {
+							echo __('<p>&copy; <a href="http://olymbook.com/">Olymbook.com. </a>','crunchpress'). __('All rights reserved.','crunchpress');
+							}
+					?>
+					</span>
 					<div class="clearfix"></div>
 					<div class="social">
 						<ul id="footer-social" class="clr">
-				<li><a href="#" target="_blank" class="facebook-icon">a</a></li>
-				<li><a href="#" target="_blank" class="twiter-icon">a</a></li>
-				<li><a href="#" target="_blank" class="pint-icon">a</a></li>
-				<li><a href="#" target="_blank" class="youtube-icon">a</a></li>
-				<li><a href="#" target="_blank" class="mail-icon">a</a></li>
-				
-			</ul>
+							<li><a href="#" target="_blank" class="facebook-icon">facebook</a></li>
+							<li><a href="#" target="_blank" class="twiter-icon">twiter</a></li>
+							<li><a href="#" target="_blank" class="pint-icon">pint</a></li>
+							<li><a href="#" target="_blank" class="youtube-icon">youtube</a></li>
+							<li><a href="#" target="_blank" class="mail-icon">mail</a></li>
+							
+						</ul>
 					</div>
 					</div>
 				</div>
@@ -50,7 +51,7 @@
 					 <b class="color-1">Bạn không muốn bỏ lỡ những thông tin hấp dẫn nhất?</b>
 						<form class="mt-15">
 							<div class="">
-								<label class="text-normal color-1" for="email">Đăng ký để nhận NewLetter hàng tuần</label>
+								<label class="text-normal color-1" for="email">Đăng ký ngay để nhận NewLetter hàng tuần</label>
 								<input type="email" class="text-input-custom" name="email" id="email" placeholder="Email của bạn" width="200"/><br>
 								<input class="mt-15 btn-submit-custom" type="submit" value="đăng ký">
 							</div>
