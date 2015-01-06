95e8e62118691f9cdf573fdd9799e4f73def0041
yukixboy <amhon009@gmail.com>
2015-01-06 23:01:36 +0700

    lấy danh sách khóa học
    lấy danh sách khóa học

--- book-store/template-parts/trainning.php
+++ book-store/template-parts/trainning.php
@@ -1,7 +1,7 @@
 <?php 
 	$args = array(
 		'posts_per_page' => 100,
-		'product_cat' => 'bookcase',
+		'product_cat' => 'course',
 		'post_type' => 'product',
 	);
 	$loop = new WP_Query( $args );
@@ -17,20 +17,32 @@
 		<div class="item <?php if ($i ==0) echo "active";?>">
 			<!-- image-->
 			<div class="span8 left-store">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/news.png"/>
+				<?php 
+				if(has_post_thumbnail( $loop->post->ID )){
+					//echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog');
+					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $loop->post->ID ), '717x355' );
+					if(isset($image[0])){
+						echo "<a href=\"".get_permalink( $loop->post->ID )."\"><img src=\"{$image[0]}\"/></a>";
+					}else{
+						echo '<img src="'.woocommerce_placeholder_img_src().'" alt="product"/>';
+					}
+				}else{
+					echo '<img src="'.woocommerce_placeholder_img_src().'" alt="product"/>';
+				}
+				?>
 			</div><!--/*image*/-->
 			<!-- content-->
 			<div class="span4 right-store">
-				<p class="t-tini text-uppercase">2 NGÀY CUỐI TUẦN SẼ LẤP ĐẦY THÀNH CÔNG TOÀN BỘ 07 KHÍA CẠNH CUỘC SỐNG CỦA BẠN</p>
+				<p class="t-tini text-uppercase"><?php echo $product->post->post_title;?></p>
 				
 				<div class="content-store-text">
-					<p class="text-justify">Nổi tiếng với câu chuyện bữa tối 21 xu & nỗ lực vươn lên từ khốn khó - Jack Canfield đã làm bừng tỉnh thế giới về niềm tin vào khoa học thành công. Trở thành Tiến sỹ/ Giảng viên của Đại học Harvard chưa làm dừng bước chân ông. Khám phá & ứng dụng bí mật Luật hấp dẫn trong thực tiễn, ônục thế giới về xuất bản sách với trên 225 tựa Chicken Soup for the Soul - bán được trên 25 triệu ấn bản...</p>
+					<p class="text-justify"><?php echo $product->post->post_content;?></p>
 
 					<div class="clearfix"></div>
 				</div>
 				<div class="clearfix"></div>
 				<div class="view-more-c">
-					<a href="#">Xem thêm </a>
+					<a href="<?php echo get_permalink( $loop->post->ID );?>">Xem thêm </a>
 				</div>
 			</div><!--/*content*/-->
 		</div>
@@ -42,7 +54,7 @@
 		</div>
 		<div class="clearfix"></div>
 	<div class=" height-75 nav-view-more row-fluid relative">
-		<a href="#" class="view-more">Xem thêm <span class="text-uppercase">Danh mục giải pháp tủ sách(10)</span></a>
+		<a href="<?php echo get_term_link("course", 'product_cat' )?>" class="view-more">Xem thêm <span class="text-uppercase">Khóa Học</span></a>
 		<a class="right arrow-icon text-transparent" href="#">#</a>
 	</div>
 </div>
