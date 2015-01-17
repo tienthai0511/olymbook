d174972afb407424d3cd59e7aab3a3833b86a30a
yukixboy <amhon009@gmail.com>
2015-01-17 09:48:47 +0700

    Update opt-in form
    Update opt-in form

--- book-store/template-parts/header-banner.php
+++ book-store/template-parts/header-banner.php
@@ -29,143 +29,33 @@
 			</div>
 		<div style="clear:both"></div>
 		<div class="witdh-1075" class="span12" style="background:#404042">
+		<div style="margin:0 auto">
 			<?php 
 			$styleOptinForm= get_option('styleOptinForm');
 			$description= get_option('description');
 			$shortDescription= get_option('shortDescription');
-			
 			if($styleOptinForm == 1){
 			?>
-			<div style="margin:0 auto">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/banner1.png">
-				<?php echo $description."<br/>".$shortDescription;?>
-				<form method="POST" class="infusion-form" action="http://email.olymbook.com/form.php?form=1" accept-charset="UTF-8" id="UserOptinForm">
-				<table width="280" cellspacing="0" cellpadding="5" border="0" align="center" background-color="#ffff00">
-					<tbody>
-						<tr>
-							<td width="99"><label for="CustomFields_2_1">First Name*</label></td>
-							<td width="181"><input type="text" name="CustomFields[2]" id="CustomFields_2_1" class="infusion-field-input-container"></td>
-						</tr>
-						<tr>
-							<td><label for="inf_field_FirstName">Your Email*</label></td>
-							<td>
-								<input type="text" name="email" id="email" class="infusion-field-input-container">
-								<input type="hidden" name="format" value="h">
-							</td>
-						</tr>
-						<tr>
-							<td style="text-align: center" colspan="2">
-								<input type="image" border="0" value="Start Now!" src="images/popup/start.jpg" name="submit">
-							</td>
-						</tr>
-					</tbody>
-				</table>
-				<p style="text-align: center;">
-				<em style="font-size: 12px">We respect your email privacy. </em>
-					</p>
-				</form>
-			</div>
 			<?php 
 			}elseif($styleOptinForm == 2){
 			?>
-			<div style="margin:0 auto">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/banner2.png">
-				<?php echo $description."<br/>".$shortDescription;?>
-				<form method="POST" class="infusion-form" action="http://email.olymbook.com/form.php?form=1" accept-charset="UTF-8" id="UserOptinForm">
-				<table width="280" cellspacing="0" cellpadding="5" border="0" align="center" background-color="#ffff00">
-					<tbody>
-						<tr>
-							<td width="99"><label for="CustomFields_2_1">First Name*</label></td>
-							<td width="181"><input type="text" name="CustomFields[2]" id="CustomFields_2_1" class="infusion-field-input-container"></td>
-						</tr>
-						<tr>
-							<td><label for="inf_field_FirstName">Your Email*</label></td>
-							<td>
-								<input type="text" name="email" id="email" class="infusion-field-input-container">
-								<input type="hidden" name="format" value="h">
-							</td>
-						</tr>
-						<tr>
-							<td style="text-align: center" colspan="2">
-								<input type="image" border="0" value="Start Now!" src="images/popup/start.jpg" name="submit">
-							</td>
-						</tr>
-					</tbody>
-				</table>
-				<p style="text-align: center;">
-				<em style="font-size: 12px">We respect your email privacy. </em>
-					</p>
-				</form>
-			</div>
 			<?php 
 			}elseif($styleOptinForm == 3){
 			?>
-			<div style="margin:0 auto">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/banner3.png">
-				<?php echo $description."<br/>".$shortDescription;?>
-				<form method="POST" class="infusion-form" action="http://email.olymbook.com/form.php?form=1" accept-charset="UTF-8" id="UserOptinForm">
-				<table width="280" cellspacing="0" cellpadding="5" border="0" align="center" background-color="#ffff00">
-					<tbody>
-						<tr>
-							<td width="99"><label for="CustomFields_2_1">First Name*</label></td>
-							<td width="181"><input type="text" name="CustomFields[2]" id="CustomFields_2_1" class="infusion-field-input-container"></td>
-						</tr>
-						<tr>
-							<td><label for="inf_field_FirstName">Your Email*</label></td>
-							<td>
-								<input type="text" name="email" id="email" class="infusion-field-input-container">
-								<input type="hidden" name="format" value="h">
-							</td>
-						</tr>
-						<tr>
-							<td style="text-align: center" colspan="2">
-								<input type="image" border="0" value="Start Now!" src="images/popup/start.jpg" name="submit">
-							</td>
-						</tr>
-					</tbody>
-				</table>
-				<p style="text-align: center;">
-				<em style="font-size: 12px">We respect your email privacy. </em>
-					</p>
-				</form>
-			</div>
 			<?php 
 			}elseif($styleOptinForm == 4){
 			?>
-			<div style="margin:0 auto">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/banner4.png">
-				<?php echo $description."<br/>".$shortDescription;?>
-				<form method="POST" class="infusion-form" action="http://email.olymbook.com/form.php?form=1" accept-charset="UTF-8" id="UserOptinForm">
-				<table width="280" cellspacing="0" cellpadding="5" border="0" align="center" background-color="#ffff00">
-					<tbody>
-						<tr>
-							<td width="99"><label for="CustomFields_2_1">First Name*</label></td>
-							<td width="181"><input type="text" name="CustomFields[2]" id="CustomFields_2_1" class="infusion-field-input-container"></td>
-						</tr>
-						<tr>
-							<td><label for="inf_field_FirstName">Your Email*</label></td>
-							<td>
-								<input type="text" name="email" id="email" class="infusion-field-input-container">
-								<input type="hidden" name="format" value="h">
-							</td>
-						</tr>
-						<tr>
-							<td style="text-align: center" colspan="2">
-								<input type="image" border="0" value="Start Now!" src="images/popup/start.jpg" name="submit">
-							</td>
-						</tr>
-					</tbody>
-				</table>
-				<p style="text-align: center;">
-				<em style="font-size: 12px">We respect your email privacy. </em>
-					</p>
-				</form>
-			</div>
 			<?php 
 			}elseif($styleOptinForm == 5){
 			?>
-			<div style="margin:0 auto">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/banner5.png">
+			<?php 
+			}
+			?>
 				<?php echo $description."<br/>".$shortDescription;?>
 				<form method="POST" class="infusion-form" action="http://email.olymbook.com/form.php?form=1" accept-charset="UTF-8" id="UserOptinForm">
 				<table width="280" cellspacing="0" cellpadding="5" border="0" align="center" background-color="#ffff00">
@@ -193,9 +83,6 @@
 					</p>
 				</form>
 			</div>
-			<?php 
-			}
-			?>
 		</div>
 		</div>
 	</div>
