1152d8361174379e0569d227f632df9adeed4242
yukixboy <amhon009@gmail.com>
2015-01-17 23:15:49 +0700

    Update giùm thái

--- book-store/template-parts/header-banner.php
+++ book-store/template-parts/header-banner.php
@@ -28,9 +28,72 @@
 			<div style="clear:both"></div>
 			</div>
 		<div style="clear:both"></div>
-		<div class="witdh-1075" class="span12" style="background:#404042">
-			<div style="margin:0 auto">
+		<div class="span12 witdh-1075 opt-in-content" style="background:#404042">
+		<div style="margin:0 auto">
+			<?php 
+			$styleOptinForm= get_option('styleOptinForm');
+			$description= get_option('description');
+			$shortDescription= get_option('shortDescription');
+			if($styleOptinForm == 1){
+			?>
 				<img src="<?php echo get_template_directory_uri(); ?>/images/banner1.png">
+			<?php 
+			}elseif($styleOptinForm == 2){
+			?>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/banner2.png">
+			<?php 
+			}elseif($styleOptinForm == 3){
+			?>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/banner3.png">
+			<?php 
+			}elseif($styleOptinForm == 4){
+			?>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/banner4.png">
+			<?php 
+			}elseif($styleOptinForm == 5){
+			?>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/banner5.png">
+			<?php 
+			}
+			?>
+				<?php //echo $description."<br/>".$shortDescription;?>
+				<form method="POST" class="infusion-form" action="http://email.olymbook.com/form.php?form=1" accept-charset="UTF-8" id="UserOptinForm">
+				<ul class="ul-opt-in">
+					<li>
+						<p class="title-opt-in">Chỉ cần đăng kí email bạn sẽ nhận</p>
+						<p class="title-opt-in">đươc Ebook này NGAY LẬP TỨC</p>
+					</li>
+					<li><input type="text" name="email" id="email" class="infusion-field-input-container"></li>
+					<li><input type="submit" border="0" value="đăng ký" class="submit-opt-in"  name="submit"></li>
+				</ul><!--/*ul-opt-in-->
+				<input type="hidden" name="format" value="h">
+				<!--
+				<table width="280" cellspacing="0" cellpadding="5" border="0" align="center" background-color="#ffff00">
+					<tbody>
+						<tr>
+							<td width="99"><label for="CustomFields_2_1">First Name*</label></td>
+							<td width="181"><input type="text" name="CustomFields[2]" id="CustomFields_2_1" class="infusion-field-input-container"></td>
+						</tr>
+						<tr>
+							<td><label for="inf_field_FirstName">Your Email*</label></td>
+							<td>
+								<input type="text" name="email" id="email" class="infusion-field-input-container">
+								<input type="hidden" name="format" value="h">
+							</td>
+						</tr>
+						<tr>
+							<td style="text-align: center" colspan="2">
+								<input type="image" border="0" value="Start Now!" src="images/popup/start.jpg" name="submit">
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				-->
+				<!--<p style="text-align: center;">
+				<em style="font-size: 12px">We respect your email privacy. </em>
+					</p>
+					-->
+				</form>
 			</div>
 		</div>
 		</div>
