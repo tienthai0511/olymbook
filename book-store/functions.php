e44a9b2e7586a0857e0b8e5adfcf5884ce0df843
yukixboy <amhon009@gmail.com>
2015-02-04 16:03:49 +0700

    Remove log write

--- book-store/functions.php
+++ book-store/functions.php
@@ -235,14 +235,3 @@ function olymbook_search() {
 
 add_action("wp_ajax_olymbook_search", "olymbook_search");
 add_action("wp_ajax_nopriv_olymbook_search", "olymbook_search");
-
-add_action('shutdown', 'sql_logger');
-function sql_logger() {
-    global $wpdb;
-    $log_file = fopen(get_template_directory().'/sql.log', 'a');
-    fwrite($log_file, "//////////////////////////////////////////\n\n" . date("F j, Y, g:i:sa" )."\n");
-    foreach($wpdb->queries as $q) {
-        fwrite($log_file, $q[0] . " - ($q[1] s)" . "\n\n");
-    }
-    fclose($log_file);
-}
