da3395700516d1f960c57d83cb63a61577ddc09f
tienthai0511 <tienthai0511@gmail.com>
2015-02-05 08:20:17 +0700

    format code
    format code

--- book-store/js/olymbook.js
+++ book-store/js/olymbook.js
@@ -1,66 +1,58 @@
-var nav = jQuery('.sort-content'),
-	animateTime = 300;
+var  animateTime = 300;
 function search(attr, value){
 	arrSearch = new Array();
 	text = jQuery('#' + attr).text();
+	element_li = '<li class="term-tag" id=\"del_' + attr + '\" data-search=\"' + attr + '=' + value + '\" data-value=\"' + value + '\" onclick="javascript:removeSeach(\'' + attr + '\', \'' + value +  '\')"><i class="term-tag-close"></i><span href="javascript:void(0);">' + text + '</span></li>';
 
-	if(jQuery('#'+attr).hasClass('added-filter')) return false;
+	if (jQuery('#' + attr).hasClass('added-filter')) return false;
 
-	element_li = '<li class="term-tag" id=\"del_'+attr+'\" data-search=\"'+attr+':'+value+'\" data-value=\"'+value+'\" onclick="javascript:removeSeach(\''+ attr+ '\', \''+value+ '\')"><i class="term-tag-close"></i><span href="javascript:void(0);">' + text + '</span></li>';
-	
 	(jQuery('.tag-filter-cd ul li').length > 0) ? jQuery('.tag-filter-cd ul li:last-child').after(element_li) : jQuery('.tag-filter-cd ul').html(element_li);
 	autoHeightAnimate(jQuery('.sort-content'), animateTime);
-	jQuery('.features-condition #'+attr).addClass('added-filter');
-	
-	jQuery('.tag-filter-cd li.term-tag').each(function(index){
+	jQuery('.features-condition #' + attr).addClass('added-filter');
+
+	jQuery('.tag-filter-cd li.term-tag').each(function(index) {
 		arrSearch.push(jQuery(this).attr('data-search'));
 	});
 	//console.log(arrSearch);
 	ajaxSearch(arrSearch);
-	arrSearch = new Array();
 	return false;
 }
 
 function removeSeach(attr, value){
 	arrSearch = new Array();
 	jQuery('.tag-filter-cd li.term-tag').each(function(index){
-		if (jQuery(this).attr('data-search') != attr +':' + value)
+	if (jQuery(this).attr('data-search') != attr +'=' + value)
 		arrSearch.push(jQuery(this).attr('data-search'));
 	});
-	jQuery('#del_'+attr).remove();
+	jQuery('#del_' + attr).remove();
 	autoHeightAnimate(jQuery('.sort-content'), animateTime);
-	jQuery('.features-condition #'+attr).removeClass('added-filter');
+	jQuery('.features-condition #' + attr).removeClass('added-filter');
 	ajaxSearch(arrSearch);
-	arrSearch = new Array();
 	return false;
 }
 
 function addSort(){
 	arrSearch = new Array();
-	
 	jQuery('.tag-filter-cd li.term-tag').each(function(index){
 		arrSearch.push(jQuery(this).attr('data-search'));
 	});
 	ajaxSearch(arrSearch);
-	arrSearch = new Array();
 	return false;
 }
 
-	function ajaxSearch(data) {
-		
-		var url      = window.location.href; 
-		urlString = data.join('&');
-		urlString = urlString.replace(/:/g, "=");
-		url_after = url.split('?')[0];
-		var orderBy = jQuery( ".orderby" ).val()
-		if(urlString == "")
-			urlString = "orderBy="+orderBy;
-		else
-			urlString += "&order_by="+orderBy;
+function ajaxSearch(data) {
+	var url = window.location.href; 
+	urlString = data.join('&');
+	url_after = url.split('?')[0];
+	var orderBy = jQuery( ".orderby" ).val()
+	if (urlString == "")
+		urlString = "orderby=" + orderBy;
+	else
+		urlString += "&order_by="+orderBy;
 
-		urlString = url_after + '?' + urlString;
-		jQuery('.over-lay').show();
-		jQuery.ajax({
+	urlString = url_after + '?' + urlString;
+	jQuery('.over-lay').show();
+	jQuery.ajax({
 		type : "post",
 		dataType : "json",
 		url : olymbookAjax.ajaxurl,
@@ -68,21 +60,19 @@ function addSort(){
 		success: function(response) {
 			if (response.sucess == true) {
 				jQuery('.over-lay').hide();
-
 				jQuery('.grid-holder.features-books').html(response.html);
-				
-				window.history.pushState(null,null,urlString);
+				window.history.pushState(null, null, urlString);
 			}
 		}
 	});
-	}
-	function autoHeightAnimate( element, time ) {
-		var curHeight = element.height(), // Get Default Height
-		autoHeight = element.css('height', 'auto').height(); // Get Auto Height
-		element.height(curHeight); // Reset to Default Height
-		element.stop().animate({ height: autoHeight }, parseInt(time)); // Animate to Auto Height
-	}
-	
+}
+
+function autoHeightAnimate( element, time ) {
+	var curHeight = element.height(), // Get Default Height
+	autoHeight = element.css('height', 'auto').height(); // Get Auto Height
+	element.height(curHeight); // Reset to Default Height
+	element.stop().animate({ height: autoHeight }, parseInt(time)); // Animate to Auto Height
+}
 jQuery(document).ready( function($) {
 	// show / hide search content 
 	var nav = $('.sort-content'),
@@ -109,29 +99,4 @@ jQuery(document).ready( function($) {
 	jQuery( ".orderby" ).change(function() {
 		addSort();
 	});
-	// get height element
-
-
-	//click search condition
-	/*$('.col-filter').each(function(){
-		$(this).bind('click', function(){
-			condition = 11;
-			data_name = $(this).attr('data-name');
-			data_value = $(this).attr('data-value');
-			data={};
-			text = $(this).text();
-			jQuery('.over-lay').show();
-			element_li = '<li class="term-tag" data-name="' + data_name + '" data-value="' + data_value + '"><i class="term-tag-close"></i><span href="#">' + text + '</span></li>';
-			($('.tag-filter-cd ul li').length > 0) ? $('.tag-filter-cd ul li:last-child').after(element_li) : $('.tag-filter-cd ul').html(element_li);
-			ajaxSearch(data_name, data_value, data, $(this), 'add');
-		});
-	});*/
-
-	// remove condition
-	/*$(document).on('click','.term-tag-close', function(){
-		$(this).parent().remove();
-			var condition = 1;//jQuery(this).attr("post_id");
-			//jQuery('.over-lay').show();
-			ajaxSearch(data_name, data_value, data, $(this), 'del');
-	});*/
 });
